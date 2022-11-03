5d0873dcf9c1d94017f9597f3eb8596644d8fd4f
NatLey30 <112613481+NatLey30@users.noreply.github.com>
2022-11-03 13:53:19 +0100

    .

--- analisis_datos.py
+++ analisis_datos.py
@@ -1,31 +1,35 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
 
+def archivo(fichero):
+    df = pd.read_csv(fichero, sep = ',', encoding = 'LATIN1')
+    fichero = ET.SubElement(root, 'fichero', {'nombre': fichero})
+
+    nans = str(df.isna().sum().sum())
+    NaN = ET.SubElement(fichero, 'NaN_en_todo_el_fichero', {'NaN': nans})
+
+    nulls = str(df.isnull().sum().sum())
+    Null = ET.SubElement(fichero, 'Null_en_todo_el_fichero', {'Null': nulls})
+
+    columnas = df.columns.values
+    for colum in range(len(columnas)):
+
+        nombre = columnas[colum]
+        columna = ET.SubElement(fichero, 'columna', {'nombre': nombre})
+
+        tipo_col = str(df[columnas[colum]].dtype)
+        tipo_columna = ET.SubElement(columna, 'tipo_dato', {'tipo': tipo_col})
 
 if __name__ == "__main__":
 
     ficheros_analizar = ['order_details.csv', 'orders.csv', 'data_dictionary.csv', 'pizzas.csv', 'pizza_types.csv']
     root = ET.Element('Análisis de NaN, null y tipologias')
 
-    for fichero in ficheros_analizar:
-
-        df = pd.read_csv(fichero, sep = ',', encoding = 'LATIN1')
-        fichero = ET.SubElement(root, 'fichero', {'nombre': fichero})
-
-        nans = str(df.isna().sum().sum())
-        NaN = ET.SubElement(fichero, 'NaN_en_todo_el fichero', {'NaN': nans})
-
-        nulls = str(df.isnull().sum().sum())
-        Null = ET.SubElement(fichero, 'Null_en_todo_el_fichero', {'Null': nulls})
-
-        columnas = df.columns.values
-        for colum in range(len(columnas)):
-
-            nombre = columnas[colum]
-            columna = ET.SubElement(fichero, 'columna', {'nombre': nombre})
-
-            tipo_col = str(df[columnas[colum]].dtype)
-            tipo_columna = ET.SubElement(columna, 'tipo_dato', {'tipo': tipo_col})
+    fichero = archivo('order_details.csv')
+    fichero = archivo('orders.csv')
+    fichero = archivo('data_dictionary.csv')
+    fichero = archivo('pizzas.csv')
+    fichero = archivo('pizza_types.csv')
 
     # escribimos en el xml
     tree = ET.ElementTree(root)
