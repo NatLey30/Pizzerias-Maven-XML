7f667f9a98024b2e437efedddbeb7e54e461f97f
NatLey30 <112613481+NatLey30@users.noreply.github.com>
2022-11-14 18:04:38 +0100

    .

--- analisis_datos.py
+++ analisis_datos.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
+from lxml import etree
 
 def archivo(fichero):
     df = pd.read_csv(fichero, sep = ',', encoding = 'LATIN1')
     fichero = ET.SubElement(root, 'fichero', {'nombre': fichero})
 
     nans = str(df.isna().sum().sum())
-    NaN = ET.SubElement(fichero, 'NaN_en_todo_el_fichero', {'NaN': nans})
+    NaN = ET.SubElement(fichero, 'NaN_en_todo_el_fichero', {'nº': nans})
 
     nulls = str(df.isnull().sum().sum())
-    Null = ET.SubElement(fichero, 'Null_en_todo_el_fichero', {'Null': nulls})
+    Null = ET.SubElement(fichero, 'Null_en_todo_el_fichero', {'nº': nulls})
 
     columnas = df.columns.values
     for colum in range(len(columnas)):
@@ -22,8 +23,7 @@ def archivo(fichero):
 
 if __name__ == "__main__":
 
-    ficheros_analizar = ['order_details.csv', 'orders.csv', 'data_dictionary.csv', 'pizzas.csv', 'pizza_types.csv']
-    root = ET.Element('Análisis de NaN, null y tipologias')
+    root = ET.Element('Análisis')
 
     fichero = archivo('order_details.csv')
     fichero = archivo('orders.csv')
@@ -32,5 +32,8 @@ if __name__ == "__main__":
     fichero = archivo('pizza_types.csv')
 
     # escribimos en el xml
+
     tree = ET.ElementTree(root)
+    ET.indent(tree)
     tree.write('Analisis_datos.xml', xml_declaration=True, encoding='utf-8')
+
