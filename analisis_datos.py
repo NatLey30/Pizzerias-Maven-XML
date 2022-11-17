66f98f35a48389432fabb33b80b424e17c55c6b5
NatLey30 <112613481+NatLey30@users.noreply.github.com>
2022-11-17 13:09:22 +0100

    .

--- analisis_datos.py
+++ analisis_datos.py
@@ -1,25 +1,45 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
-from lxml import etree
+
 
 def archivo(fichero):
+    '''
+    Función que va a analizar cada archivo buscando cuantos
+    Nans y Nulls hay en todo el csv. También va a estudiar la
+    tipología de cada columna, a la vaz que mira cuantos
+    Nans y Nulls hay en esta.
+    '''
     df = pd.read_csv(fichero, sep = ',', encoding = 'LATIN1')
     fichero = ET.SubElement(root, 'fichero', {'nombre': fichero})
 
+    # Nans totales
     nans = str(df.isna().sum().sum())
     ET.SubElement(fichero, 'NaN_en_todo_el_fichero', {'nº': nans})
 
+    # Nulls totales
     nulls = str(df.isnull().sum().sum())
     ET.SubElement(fichero, 'Null_en_todo_el_fichero', {'nº': nulls})
 
+    # Sacamos columnas
     columnas = df.columns.values
-    for colum in range(len(columnas)):
 
+    # Para cada columna
+    for colum in range(len(columnas)):
         nombre = columnas[colum]
         columna = ET.SubElement(fichero, 'columna', {'nombre': nombre})
 
+        # Buscamos su tipología
         tipo_col = str(df[columnas[colum]].dtype)
         ET.SubElement(columna, 'tipo_dato', {'tipo': tipo_col})
+
+        # Número de nans
+        nans_col = str(df[columnas[colum]].isna().sum().sum())
+        ET.SubElement(columna, 'nans_columna', {'nº': nans_col})
+
+        # Número de nulls
+        nulls_col = str(df[columnas[colum]].isnull().sum().sum())
+        ET.SubElement(columna, 'nulls_columna', {'nº': nulls_col})
+
 
 if __name__ == "__main__":
 
@@ -31,8 +51,7 @@ if __name__ == "__main__":
     fichero = archivo('pizzas.csv')
     fichero = archivo('pizza_types.csv')
 
-    # escribimos en el xml
-
+    # Escribimos en el xml
     tree = ET.ElementTree(root)
     ET.indent(tree)
-    tree.write('Analisis_datos.xml', xml_declaration=True, encoding='utf-8')
+    tree.write('analisis_datos.xml', xml_declaration=True, encoding='utf-8')
